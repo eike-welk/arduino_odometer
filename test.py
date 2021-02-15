bc22438f4d44f44a6afd08490d3d95e06e46ae00
Eike Welk <eike.welk@gmx.net>
2021-02-15 01:59:18 +0000

    Adapted test program to new firmware.

--- test.py
+++ test.py
@@ -13,30 +13,33 @@ i2c = Adafruit_PureIO.smbus.SMBus(1)
 
 def read_reg_who():
     """Read the who-am-I register."""
-    #ans_who = i2c.read_byte_data(address, reg_who)
-    buf = i2c.read_i2c_block_data(address, reg_who, 1)
-    ans_who, = struct.unpack(">b", buf)
-    print("Who am I: " + hex(ans_who))
+    buf = i2c.read_i2c_block_data(address, reg_who, 7)
+    print(buf)
+    ans_who, = struct.unpack("!6sx", buf)
+    ans_who_str = ans_who.decode('utf-8')
+    print("Who am I: ", ans_who_str)
 
 def read_reg_counters():
     """Read the counter registers."""
-    buf = i2c.read_i2c_block_data(address, reg_counts, 8)
-    counter_1, counter_2 = struct.unpack(">ii", buf)
-    print('Counters 1:', counter_1, ', 2:', counter_2)
+    buf = i2c.read_i2c_block_data(address, reg_counts, 16)
+    counter_1, counter_2, counter_3, counter_4 = struct.unpack('!4i', buf)
+    print('Counters 1:', counter_1, ', 2:', counter_2, ', 3:', counter_3, ', 4:', counter_4)
 
 def write_reg_reset(val):
     """Write the reset register."""
-    buf = struct.pack(">i", val)
+    buf = struct.pack("!i", val)
     i2c.write_i2c_block_data(address, reg_reset, buf)
 
 read_reg_who()
-time.sleep(0.9)
+#time.sleep(0.9)
+write_reg_reset(0)
+#time.sleep(0.1)
 
-read_reg_counters()
-time.sleep(0.05)
-read_reg_counters()
+while True:
+    read_reg_counters()
+    time.sleep(0.05)
+    #time.sleep(0.01)
 
-write_reg_reset(5)
 time.sleep(0.1)
 
 #read_reg_counters()
